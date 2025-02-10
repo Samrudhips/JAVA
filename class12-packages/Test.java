18e15a77da21636e1929179afcd87ec10ae45867
Samrudhips <20cs134.samrudhi@sjec.ac.in>
2025-02-10 13:15:41 +0530

    10-02-2025

---
+++ class12-packages/Test.java
@@ -0,0 +1,11 @@
+package com.fk.users;// 3 folders will be created
+//execute javac -d . Test.java(compile)
+//Run java com.fk.users.Test
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("GM");
+
+        
+    }
+    
+}
