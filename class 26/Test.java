d251b75b8c207dc54432fda4cd0f66d79b76f96e
Samrudhips <20cs134.samrudhi@sjec.ac.in>
2025-03-12 13:10:36 +0530

    12/03/2025

---
+++ class 26/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        int []eids=new int[4];
+        eids[0]=101;
+        eids[2]=103;
+        System.out.println(eids);
+        System.out.println(eids[0]);
+        System.out.println(eids[1]);
+        System.out.println(eids[2]);
+        System.out.println(eids[3]);
+    }
+    
+}
